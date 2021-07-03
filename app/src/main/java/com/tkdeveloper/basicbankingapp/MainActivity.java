18b827fd95d75e4f78d27eab2977da05812b3a28
tushar-kulwade <tushar61068>
2021-07-04 00:43:52 +0530

    my project

---
+++ app/src/main/java/com/tkdeveloper/basicbankingapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.tkdeveloper.basicbankingapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+ImageView customer,transaction;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        customer=findViewById(R.id.customer);
+        transaction=findViewById(R.id.Transactions);
+
+        customer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(com.tkdeveloper.basicbankingapp.MainActivity.this,user_list.class));
+            }
+        });
+        transaction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(com.tkdeveloper.basicbankingapp.MainActivity.this, com.tkdeveloper.basicbankingapp.history_list.class));
+            }
+        });
+    }
+}
